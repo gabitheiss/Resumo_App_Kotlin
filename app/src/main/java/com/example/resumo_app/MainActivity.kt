53c3f1f4a326648e9032b27297abbddd0d179532
gabitheiss <t_gaaby@hotmail.com>
2021-09-14 13:27:29 -0300

    Função de voltar - botão back

--- app/src/main/java/com/example/resumo_app/MainActivity.kt
+++ app/src/main/java/com/example/resumo_app/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.resumo_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration : AppBarConfiguration
+    private lateinit var navController: NavController
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //estanciando as variaveis para criar a funcao de voltar - abaixo
+        navController = findNavController(R.id.navHost)
+        appBarConfiguration = AppBarConfiguration.Builder(navController.graph).build()
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
     }
+
+    //funcao para voltar (botao superior)
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
 }
